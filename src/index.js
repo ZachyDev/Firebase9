251a172387edef90d0b5e2bc7241ffe3c68fec9f
= <=>
2022-02-24 09:37:17 +0300

    Getting Started with Firebase 9 #5 - Adding & Deleting Documents

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 // import firebase library
 import { initializeApp } from 'firebase/app';
 
+// import Firestore functions
+import { 
+    getFirestore,
+    collection,
+    getDocs,
+} from 'firebase/firestore';
+
+// projwct config
 const firebaseConfig = {
-    apiKey: "AIzaSyA81fq2bHjctsjfUXd2i64b3AG1tVioSTU",
-    authDomain: "therapist-connect.firebaseapp.com",
-    projectId: "therapist-connect",
-    storageBucket: "therapist-connect.appspot.com",
-    messagingSenderId: "308490251184",
-    appId: "1:308490251184:web:57251778e09fe87b164475"
+    apiKey: "AIzaSyDgFT_a4AFYV17_yeMlwQOEZZGJ71Rw2Gk",
+    authDomain: "fir-9-do-6b87f.firebaseapp.com",
+    projectId: "fir-9-do-6b87f",
+    storageBucket: "fir-9-do-6b87f.appspot.com",
+    messagingSenderId: "926747255617",
+    appId: "1:926747255617:web:8870b670471dd9fa26b7c0"
 };
 
 // connect to Firebase back end
 initializeApp(firebaseConfig);
+
+// initialize services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'students');
+
+// get documents
+getDocs(colRef)
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            let students = doc;
+            let docId = students.id;
+            console.log(students.data());
+            console.log(docId)
+        })
+    })
